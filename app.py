0848b6929631967ded1f6ee1cbb4ef3e8ba85c09
savaseiv-create <savaseiv@gmail.com>
2026-03-23 01:47:58 +0000

    Fix indentation in about route

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return "<h1>About page</h1>"
+    return "<h1>About page</h1>" 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9090)
